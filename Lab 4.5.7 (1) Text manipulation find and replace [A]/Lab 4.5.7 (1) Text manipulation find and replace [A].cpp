2bb64062ad7657f4d2b2bc223abbb7b03e938af0
Andrew <mooweekandrew@gmail.com>
2021-01-03 09:41:28 +0200

    Added Lab 4.5.7 (1) Text manipulation find and replace [A]

--- Lab 4.5.7 (1) Text manipulation find and replace [A]/Lab 4.5.7 (1) Text manipulation find and replace [A].cpp
+++ Lab 4.5.7 (1) Text manipulation find and replace [A]/Lab 4.5.7 (1) Text manipulation find and replace [A].cpp
@@ -7,17 +7,23 @@ int main()
 
 
 	// manipulate the sentence here
-	std::string sent = sentence;
+	std::string newSent = "";
 
 	while (sentence.find(' ') != -1) 
 	{
-		std::string word = sentence.substr(0, sentence.find(' '));
+		std::string fWord = sentence.substr(0, sentence.find(' '));
 		sentence = sentence.substr(sentence.find(' ') + 1);
-		if (sentence.find(' ') == -1)
+		if (sentence.find(' ') == -1) {
+			newSent += fWord + " ";
 			break;
+		}
 		std::string sWord = sentence.substr(0, sentence.find(' '));
+		if (fWord == sWord) {
+			sentence = sentence.substr(sentence.find(' ') + 1);
+		}
+		newSent += fWord + " ";
 	}
+	newSent += sentence;
 
-
-	std::cout << sentence << "\n";
+	std::cout << newSent << "\n";
 }
